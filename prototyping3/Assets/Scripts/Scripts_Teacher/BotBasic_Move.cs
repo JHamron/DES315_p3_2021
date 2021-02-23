97eac3f057304adbda9631a4f58652b0bd06edc2
GregBurrill <GregBurrillWeb@gmail.com>
2021-02-22 22:31:04 -0500

    Update BotBasic_Move.cs

--- prototyping3/Assets/Scripts/Scripts_Teacher/BotBasic_Move.cs
+++ prototyping3/Assets/Scripts/Scripts_Teacher/BotBasic_Move.cs
@@ -18,6 +18,12 @@ public class BotBasic_Move : MonoBehaviour
 	public bool isGrounded;
 	public bool isTurtled;
 
+	// flip cooldown logic
+	public bool canFlip = true;
+	// private bool canFlipGate = true;
+	// private float flipTimer = 0;
+	// public float flipTime = 1f;
+
 	//grab axis from parent object
 	public string parentName;
 	public string pVertical;
@@ -25,9 +31,7 @@ public class BotBasic_Move : MonoBehaviour
 	public string pJump;
 	public string button4; // right bumper or [y] or [/] keys, to test on boost
 	
-
-    void Start()
-    {
+    void Start(){
 		if (gameObject.GetComponent<Rigidbody>() != null){
 			rb = gameObject.GetComponent<Rigidbody>();
 		}
@@ -45,40 +49,61 @@ public class BotBasic_Move : MonoBehaviour
     }
 
     void Update(){
-
 		float botMove = Input.GetAxisRaw(pVertical) * moveSpeed * Time.deltaTime;
 		float botRotate = Input.GetAxisRaw(pHorizontal) * rotateSpeed * Time.deltaTime;
 		transform.Translate(0,0, botMove);
 		transform.Rotate(0, botRotate, 0);
 
 		// JUMP
-		//isGrounded = Physics2D.OverlapCircle(GroundCheck1.position, 0.15f, groundLayer);
 		isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundLayer);
 		isTurtled = Physics.CheckSphere(turtleCheck.position, 0.4f, groundLayer);
 		if (Input.GetButtonDown(pJump)){
 			if (isGrounded == true){
 				rb.AddForce(rb.centerOfMass + new Vector3(0f, jumpSpeed*10, 0f), ForceMode.Impulse);
 			}
-			else if (isTurtled == true){
+			
+			//flip cooldown logic
+			// if ((isTurtled == true) && (canFlip == false)){
+				// canFlipGate = false;	
+			// }
+			
+			if ((isTurtled == true) && (canFlip == true)){
 				rb.AddForce(rb.centerOfMass + new Vector3(jumpSpeed / 2, 0, jumpSpeed / 2), ForceMode.Impulse);
-				//rb.transform.Rotate(Vector3(flipSpeed,0,0));
-				//rb.AddTorque.transform.Rotate(Vector3(flipSpeed,0,0), ForceMode.Impulse);
 				transform.Rotate(flipSpeed, 0, 0);
+				// canFlip = false;
+				// canFlipGate = true;
 			}
-			//else gameObject.transform.rotation = gameObject.transform.parent.rotation;
-			//else transform.rotation = Quaternion.Euler(90, transform.eulerAngle.y, 90);
-			//else  transform.Rotate (new Vector3 (90, gameObject.eulerAngle.y, 90));
-			else {
+
+			else if (canFlip == true){
 				Vector3 betterEulerAngles = new Vector3(gameObject.transform.parent.eulerAngles.x, transform.eulerAngles.y, gameObject.transform.parent.eulerAngles.z); 
 				transform.eulerAngles = betterEulerAngles;
 			}
-
 		}
 
 		// BOOST
-		//if (Input.GetButton("Boost"))
-		if (Input.GetButtonDown(button4)){
-			rb.AddForce(transform.forward * boostSpeed, ForceMode.Impulse);
-		}
+		// if (Input.GetButtonDown(button4)){
+			// rb.AddForce(transform.forward * boostSpeed, ForceMode.Impulse);
+		// }
     }
+	
+	//Flip cooldown timer
+	// void FixedUpdate(){
+		// if (canFlipGate == false){
+			// flipTimer += 1f;
+			// if (flipTimer >= flipTime){
+				// canFlip = true;
+				// flipTimer = 0f;
+			// }
+		// }
+	// }
+	
 }
+
+
+
+	//isGrounded = Physics2D.OverlapCircle(GroundCheck1.position, 0.15f, groundLayer);
+	//rb.transform.Rotate(Vector3(flipSpeed,0,0));
+	//rb.AddTorque.transform.Rotate(Vector3(flipSpeed,0,0), ForceMode.Impulse);
+	//else gameObject.transform.rotation = gameObject.transform.parent.rotation;
+	//else transform.rotation = Quaternion.Euler(90, transform.eulerAngle.y, 90);
+	//else  transform.Rotate (new Vector3 (90, gameObject.eulerAngle.y, 90));
