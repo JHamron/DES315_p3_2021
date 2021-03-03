11e6dd6ffda8312edb80d8aa892549f5649ade60
Austin Stead <austin.s@digipen.edu>
2021-03-02 22:54:12 -0800

    Script Setup and Shield Regen
    Copied over bot related base scripts, and focused on damage handler. Shield Regen works on the bot end, but not hooked up to UI.

---
+++ prototyping3/Assets/Scripts/Scripts_Students/AustinStead/BotB04_DamageHandler.cs
@@ -0,0 +1,351 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace BotB04.Controller
+{
+	public class BotB04_DamageHandler : MonoBehaviour
+	{
+		public GameObject compassSides;
+		public GameObject compassVertical;
+		private float sidelimit = 3.0f;
+		private float attackDamage;
+		public float knockBackSpeed = 10f;
+
+		[System.Serializable]
+        public class ShieldData
+        {
+			public float RechargeDelay = 2f;
+			public float RechargeRate  = 0f;
+
+			public float PowerFrontMax  = 5f;
+			public float PowerBackMax   = 5f;
+			public float PowerLeftMax   = 5f;
+			public float PowerRightMax  = 5f;
+			public float PowerTopMax    = 5f;
+			public float PowerBottomMax = 5f;
+		}
+		[SerializeField]
+		public ShieldData ShieldBaseStats;
+
+		public class ShieldRuntimeData
+		{
+			public float powerFront;
+			public float powerBack;
+			public float powerLeft;
+			public float powerRight;
+			public float powerTop;
+			public float powerBottom;
+
+			public float delayFront;
+			public float delayBack;
+			public float delayLeft;
+			public float delayRight;
+			public float delayTop;
+			public float delayBottom;
+
+			public ShieldRuntimeData(ShieldData ShieldBaseStats)
+            {
+				powerFront  = ShieldBaseStats.PowerFrontMax;
+				powerBack   = ShieldBaseStats.PowerBackMax;
+				powerLeft   = ShieldBaseStats.PowerLeftMax;
+				powerRight  = ShieldBaseStats.PowerRightMax;
+				powerTop    = ShieldBaseStats.PowerTopMax;
+				powerBottom = ShieldBaseStats.PowerBottomMax;
+
+				delayFront  = 0;
+				delayBack   = 0;
+				delayLeft   = 0;
+				delayRight  = 0;
+				delayTop    = 0;
+				delayBottom = 0;
+			}
+
+		}
+		private ShieldRuntimeData shieldRuntime;
+
+		[System.Serializable]
+        public class ShieldReferenceStorage
+        {
+			public GameObject shieldFrontObj;
+			public GameObject shieldBackObj;
+			public GameObject shieldLeftObj;
+			public GameObject shieldRightObj;
+			public GameObject shieldTopObj;
+			public GameObject shieldBottomObj;
+		}
+		[SerializeField]
+		public ShieldReferenceStorage ShieldReferences;
+
+		[System.Serializable]
+        public class DamageParticleStorage
+        {
+			public GameObject dmgParticlesFront;
+			public GameObject dmgParticlesBack;
+			public GameObject dmgParticlesLeft;
+			public GameObject dmgParticlesRight;
+			public GameObject dmgParticlesTop;
+		}
+		[SerializeField]
+		public DamageParticleStorage DamageParticleReferences;
+
+		private Rigidbody rb;
+		private GameHandler gameHandler;
+		private string thisPlayer;
+		private bool notMyWeapon = true;
+
+		void Start()
+		{
+			if (gameObject.GetComponent<Rigidbody>() != null)
+			{
+				rb = gameObject.GetComponent<Rigidbody>();
+			}
+			if (GameObject.FindWithTag("GameHandler") != null)
+			{
+				gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+			}
+			thisPlayer = gameObject.transform.root.tag;
+
+			ShieldReferences.shieldFrontObj.SetActive(false);
+			ShieldReferences.shieldBackObj.SetActive(false);
+			ShieldReferences.shieldLeftObj.SetActive(false);
+			ShieldReferences.shieldRightObj.SetActive(false);
+			ShieldReferences.shieldTopObj.SetActive(false);
+			ShieldReferences.shieldBottomObj.SetActive(false);
+
+			shieldRuntime = new ShieldRuntimeData(ShieldBaseStats);
+
+			DamageParticleReferences.dmgParticlesFront.SetActive(false);
+			DamageParticleReferences.dmgParticlesBack.SetActive(false);
+			DamageParticleReferences.dmgParticlesLeft.SetActive(false);
+			DamageParticleReferences.dmgParticlesRight.SetActive(false);
+			DamageParticleReferences.dmgParticlesTop.SetActive(false);
+
+
+		}
+
+        private void Update()
+        {
+			RepairShield(ref shieldRuntime.powerFront,  ShieldBaseStats.PowerFrontMax,  ref shieldRuntime.delayFront, DamageParticleReferences.dmgParticlesFront);
+			RepairShield(ref shieldRuntime.powerBack,   ShieldBaseStats.PowerBackMax,   ref shieldRuntime.delayBack, DamageParticleReferences.dmgParticlesBack);
+			RepairShield(ref shieldRuntime.powerLeft,   ShieldBaseStats.PowerLeftMax,   ref shieldRuntime.delayLeft, DamageParticleReferences.dmgParticlesLeft);
+			RepairShield(ref shieldRuntime.powerRight,  ShieldBaseStats.PowerRightMax,  ref shieldRuntime.delayRight, DamageParticleReferences.dmgParticlesRight);
+			RepairShield(ref shieldRuntime.powerTop,    ShieldBaseStats.PowerTopMax,    ref shieldRuntime.delayTop, DamageParticleReferences.dmgParticlesTop);
+			RepairShield(ref shieldRuntime.powerBottom, ShieldBaseStats.PowerBottomMax, ref shieldRuntime.delayBottom, null);
+		}
+
+		private void RepairShield(ref float shieldHealth, float shieldMax, ref float delay, GameObject damageParticles)
+        {
+			delay -= Time.deltaTime;
+			if(delay <= 0)
+            {
+				shieldHealth = Mathf.Min(shieldHealth + ShieldBaseStats.RechargeRate * Time.deltaTime, shieldMax);
+				if(damageParticles != null)
+					damageParticles.SetActive(false);
+			}
+		}
+
+
+
+        private void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.tag == "Hazard")
+			{
+				if ((other.gameObject.GetComponent<HazardDamage>().isPlayer1Weapon == false) && (thisPlayer == "Player1")) { notMyWeapon = true; }
+				else if ((other.gameObject.GetComponent<HazardDamage>().isPlayer2Weapon == false) && (thisPlayer == "Player2")) { notMyWeapon = true; }
+				else { notMyWeapon = false; }
+			}
+
+			if ((other.gameObject.tag == "Hazard") && (notMyWeapon == true))
+			{
+				attackDamage = other.gameObject.GetComponent<HazardDamage>().damage;
+
+				Vector3 directionFore = other.transform.position - transform.position;
+				Vector3 directionSides = other.transform.position - compassSides.transform.position;
+				Vector3 directionVert = other.transform.position - compassVertical.transform.position;
+
+				//Hit Back!!!
+				if (Vector3.Dot(transform.forward, directionFore) < (-sidelimit))
+				{
+					shieldRuntime.delayBack = ShieldBaseStats.RechargeDelay;
+
+					rb.AddForce(transform.forward * knockBackSpeed * -1, ForceMode.Impulse);
+					//Debug.Log("HitBack " + Vector3.Dot (transform.forward, directionFore));
+					if (shieldRuntime.powerBack <= 0)
+					{
+						DamageParticleReferences.dmgParticlesBack.SetActive(true);
+						//string playerDamaged = gameObject.tag; //remove for final;
+						//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+						gameHandler.TakeDamage(thisPlayer, attackDamage);  //use in final (slotted players)
+					}
+					else
+					{
+						shieldRuntime.powerBack -= attackDamage;
+						StartCoroutine(ShieldHitDisplay(ShieldReferences.shieldBackObj));
+						if (shieldRuntime.powerBack <= 0)
+						{
+							shieldRuntime.powerBack = 0;
+							//string playerDamaged = gameObject.tag; //remove for final;
+							//gameHandler.PlayerShields(playerDamaged, "Back"); //remove for final;
+							gameHandler.PlayerShields(thisPlayer, "Back");  //use in final (slotted players)
+						}
+					}
+				}
+
+				//Hit Front!!!
+				if (Vector3.Dot(transform.forward, directionFore) > sidelimit)
+				{
+					shieldRuntime.delayFront = ShieldBaseStats.RechargeDelay;
+
+					rb.AddForce(transform.forward * knockBackSpeed, ForceMode.Impulse);
+					//Debug.Log("HitFront "+ Vector3.Dot (transform.forward, directionFore));
+					if (shieldRuntime.powerFront <= 0)
+					{
+						DamageParticleReferences.dmgParticlesFront.SetActive(true);
+						//string playerDamaged = gameObject.tag; //remove for final;
+						//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+						//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+						gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+					}
+					else
+					{
+						shieldRuntime.powerFront -= attackDamage;
+						StartCoroutine(ShieldHitDisplay(ShieldReferences.shieldFrontObj));
+						if (shieldRuntime.powerFront <= 0)
+						{
+							shieldRuntime.powerFront = 0;
+							//string playerDamaged = gameObject.tag; //remove for final;
+							//gameHandler.PlayerShields(playerDamaged, "Front"); //remove for final;
+							gameHandler.PlayerShields(thisPlayer, "Front");  //use in final (slotted players)
+						}
+					}
+				}
+
+				//Hit right!!!
+				if (Vector3.Dot(compassSides.transform.forward, directionSides) > sidelimit)
+				{
+					shieldRuntime.delayRight = ShieldBaseStats.RechargeDelay;
+
+					rb.AddForce(transform.right * knockBackSpeed, ForceMode.Impulse);
+					//Debug.Log("HitRight " + Vector3.Dot (compassSides.transform.forward, directionSides));
+					if (shieldRuntime.powerRight <= 0)
+					{
+						DamageParticleReferences.dmgParticlesRight.SetActive(true);
+						//string playerDamaged = gameObject.tag; //remove for final;
+						//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+						//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+						gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+					}
+					else
+					{
+						shieldRuntime.powerRight -= attackDamage;
+						StartCoroutine(ShieldHitDisplay(ShieldReferences.shieldRightObj));
+						if (shieldRuntime.powerRight <= 0)
+						{
+							shieldRuntime.powerRight = 0;
+							//string playerDamaged = gameObject.tag; //remove for final;
+							//gameHandler.PlayerShields(playerDamaged, "Right"); //remove for final;
+							gameHandler.PlayerShields(thisPlayer, "Right");  //use in final (slotted players)
+						}
+					}
+				}
+
+				//Hit left!!!
+				if (Vector3.Dot(compassSides.transform.forward, directionSides) < (-sidelimit))
+				{
+					shieldRuntime.delayLeft = ShieldBaseStats.RechargeDelay;
+
+					rb.AddForce(transform.right * knockBackSpeed * -1, ForceMode.Impulse);
+					//Debug.Log("HitLeft " + Vector3.Dot (compassSides.transform.forward, directionSides));
+					if (shieldRuntime.powerLeft <= 0)
+					{
+						DamageParticleReferences.dmgParticlesLeft.SetActive(true);
+						//string playerDamaged = gameObject.tag; //remove for final;
+						//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+						//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+						gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+					}
+					else
+					{
+						shieldRuntime.powerLeft -= attackDamage;
+						StartCoroutine(ShieldHitDisplay(ShieldReferences.shieldLeftObj));
+						if (shieldRuntime.powerLeft <= 0)
+						{
+							shieldRuntime.powerLeft = 0;
+							//string playerDamaged = gameObject.tag; //remove for final;
+							//gameHandler.PlayerShields(playerDamaged, "Left"); //remove for final;
+							gameHandler.PlayerShields(thisPlayer, "Left");  //use in final (slotted players)
+						}
+					}
+				}
+
+				//Hit top!!!
+				if (Vector3.Dot(compassVertical.transform.forward, directionVert) > sidelimit)
+				{
+					shieldRuntime.delayTop = ShieldBaseStats.RechargeDelay;
+					
+					rb.AddForce(transform.up * knockBackSpeed, ForceMode.Impulse);
+					//Debug.Log("HitTop " + Vector3.Dot (compassVertical.transform.forward, directionVert));
+					if (shieldRuntime.powerTop <= 0)
+					{
+						DamageParticleReferences.dmgParticlesTop.SetActive(true);
+						gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+					}
+					else
+					{
+						shieldRuntime.powerTop -= attackDamage;
+						StartCoroutine(ShieldHitDisplay(ShieldReferences.shieldTopObj));
+						if (shieldRuntime.powerTop <= 0)
+						{
+							shieldRuntime.powerTop = 0;
+							gameHandler.PlayerShields(thisPlayer, "Top");  //use in final (slotted players)
+						}
+					}
+				}
+
+				//Hit bottom!!!
+				if (Vector3.Dot(compassVertical.transform.forward, directionVert) < (-sidelimit))
+				{
+					shieldRuntime.delayBottom = ShieldBaseStats.RechargeDelay;
+
+					rb.AddForce(transform.up * knockBackSpeed * -1, ForceMode.Impulse);
+					//Debug.Log("HitBottom " + Vector3.Dot (compassVertical.transform.forward, directionVert));
+					if (shieldRuntime.powerBottom <= 0)
+					{
+						//dmgParticlesBottom.SetActive(true);
+						gameHandler.TakeDamage(thisPlayer, attackDamage);
+					}
+					
+					else
+					{
+						shieldRuntime.powerBottom -= attackDamage;
+						if (shieldRuntime.powerBottom <= 0)
+						{
+							shieldRuntime.powerBottom = 0;
+							gameHandler.PlayerShields(thisPlayer, "Bottom");
+						}
+					}
+				}
+			}
+		}
+
+		IEnumerator ShieldHitDisplay(GameObject shieldObj)
+		{
+			shieldObj.SetActive(true);
+			// Renderer shieldRenderer = GetComponent<Renderer> ();
+			// shieldRenderer.material.color = new Color(255,200,0,1f);
+			yield return new WaitForSeconds(0.4f);
+			//shieldRenderer.material.color = new Color(255,200,0,0f);
+			shieldObj.SetActive(false);
+		}
+
+
+
+	
+
+
+
+
+	}
+}
---
+++ prototyping3/Assets/Scripts/Scripts_Students/AustinStead/BotB04_WeaponHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BotB04.Controller
+{
+	public class BotB04_WeaponHandler : MonoBehaviour
+	{
+		//NOTE: This script goes on the main playerBot Game Object, and the weapon goes in the public GO slot
+
+		public GameObject weaponThrust;
+		private float thrustAmount = 3f;
+
+		private bool weaponOut = false;
+
+		//grab axis from parent object
+		string button1;
+		string button2;
+		string button3;
+		string button4; // currently boost in player move script
+
+		void Start()
+		{
+			button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
+			button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
+			button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
+			button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
+		}
+
+		void Update()
+		{
+			//if (Input.GetKeyDown(KeyCode.T)){
+			if ((Input.GetButtonDown(button1)) && (weaponOut == false))
+			{
+				weaponThrust.transform.Translate(0, thrustAmount, 0);
+				weaponOut = true;
+				StartCoroutine(WithdrawWeapon());
+			}
+		}
+
+		IEnumerator WithdrawWeapon()
+		{
+			yield return new WaitForSeconds(0.6f);
+			weaponThrust.transform.Translate(0, -thrustAmount, 0);
+			weaponOut = false;
+		}
+
+	}
+}
