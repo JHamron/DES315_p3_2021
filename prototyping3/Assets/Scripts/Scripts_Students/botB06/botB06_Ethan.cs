5a7fe43ffbec894d35d13d725f977468b34d09be
DECADSM <duke129409@gmail.com>
2021-03-04 13:55:09 -0500

    Started to convert the blender model into a working thing

--- prototyping3/Assets/Scripts/Scripts_Students/botB06/botB06_Ethan.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/botB06/botB06_Ethan.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class botB06_Ethan : MonoBehaviour
 {
+    //vars pulled from teacher
+    public GameObject compassSides;
+    public GameObject compassVertical;
+    private float sidelimit = 3.0f;
+    private float attackDamage;
+    public float knockBackSpeed = 10f;
+
+    public float shieldPowerTop = 5f;
+    public float shieldPowerBottom = 5f;
+
+    private Rigidbody rb;
+    private GameHandler gameHandler;
+    private string thisPlayer;
+    private bool notMyWeapon = true;
+
+    //Shields
+    public GameObject TopShield, BottomShield;
+    //particles
+    public GameObject TopParticles, BottomParticles;
+
     // Start is called before the first frame update
     void Start()
     {
