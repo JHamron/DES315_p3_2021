eaef0f73977251c2766f7131168fdc421252e702
Amy Stoltz <amy.s@digipen.edu>
2021-03-04 17:40:41 -0800

    Added wedge to front shield for bot

---
+++ prototyping3/Assets/Scripts/Scripts_Students/botA15/botA15_Weapons.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botA15_Weapons : MonoBehaviour
+{
+	public GameObject weaponThrust;
+	public GameObject wedge;
+
+	private float thrustAmount = 3f;
+
+	private bool weaponOut = false;
+
+	//grab axis from parent object
+	public string button1;
+	public string button2;
+	public string button3;
+	public string button4; // currently boost in player move script
+
+	void Start()
+	{
+		button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
+		button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
+		button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
+		button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
+	}
+
+	void Update()
+	{
+		//if (Input.GetKeyDown(KeyCode.T)){
+		if ((Input.GetButtonDown(button1)) && (weaponOut == false))
+		{
+			weaponThrust.transform.Translate(0, thrustAmount, 0);
+			weaponOut = true;
+			StartCoroutine(WithdrawWeapon());
+		}
+	}
+
+	IEnumerator WithdrawWeapon()
+	{
+		yield return new WaitForSeconds(0.6f);
+		weaponThrust.transform.Translate(0, -thrustAmount, 0);
+		weaponOut = false;
+	}
+}
