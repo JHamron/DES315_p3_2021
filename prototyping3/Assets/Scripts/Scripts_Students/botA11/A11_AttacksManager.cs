9b0fb56f9a360c229e62de62c7362fafe53436c9
AmoghSubhedar <amogh.subhedar@digipen.edu>
2021-03-05 14:39:13 -0800

    Add nav mesh to arena

---
+++ prototyping3/Assets/Scripts/Scripts_Students/botA11/A11_AttacksManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Amogh
+{
+    public class A11_AttacksManager : MonoBehaviour
+    {
+        private string[] buttonCodes;
+
+        private A11_IAttack[] attacks;
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            var parentScript = GetComponentInParent<playerParent>();
+
+            buttonCodes = new[] { 
+                parentScript.action1Input, 
+                parentScript.action2Input, 
+                parentScript.action3Input, 
+                parentScript.action4Input 
+            };
+
+            attacks = new[]
+            {
+                GetComponent<A11_Heal>().GetComponent<A11_IAttack>(),
+                null,
+                null,
+                null
+            };
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            for (int i = 0; i < buttonCodes.Length; ++i)
+            {
+                if (Input.GetButtonDown(buttonCodes[i]))
+                {
+                    if (attacks[i] != null)
+                    {
+                        attacks[i].ButtonDown();
+                    }
+                }
+                else if (Input.GetButton(buttonCodes[i]))
+                {
+                    if (attacks[i] != null)
+                    {
+                        attacks[i].ButtonHeld();
+                    }
+                }
+                else if (Input.GetButtonUp(buttonCodes[i]))
+                {
+                    if (attacks[i] != null)
+                    {
+                        attacks[i].ButtonUp();
+                    }
+                }
+            }
+        }
+    }
+}
