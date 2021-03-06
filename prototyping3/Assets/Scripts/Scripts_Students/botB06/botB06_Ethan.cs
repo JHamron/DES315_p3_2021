a61db131dbcacd938fccdb28a87f391f2c4977eb
DECADSM <duke129409@gmail.com>
2021-03-06 17:16:41 -0500

    Shields now work using the teachers code and a working weapon again reusing the teachers code.

--- prototyping3/Assets/Scripts/Scripts_Students/botB06/botB06_Ethan.cs
+++ prototyping3/Assets/Scripts/Scripts_Students/botB06/botB06_Ethan.cs
@@ -11,8 +11,12 @@ public class botB06_Ethan : MonoBehaviour
     private float attackDamage;
     public float knockBackSpeed = 10f;
 
-    public float shieldPowerTop = 5f;
-    public float shieldPowerBottom = 5f;
+    //public float shieldPowerFront = 3f;
+    public float shieldPowerBack = 3f;
+    public float shieldPowerLeft = 3f;
+    public float shieldPowerRight = 3f;
+    public float shieldPowerTop = 3f;
+    public float shieldPowerBottom = 3f;
 
     private Rigidbody rb;
     private GameHandler gameHandler;
@@ -23,6 +27,20 @@ public class botB06_Ethan : MonoBehaviour
     public GameObject TopShield, BottomShield, LeftShield, RightShield, BackShield;
     //particles
     public GameObject TopParticles, BottomParticles, LeftParticles, RightParticles, BackParticles;
+
+    //Weapon Vars
+    public GameObject FrontWeapon;
+    private float thrustAmount = 1f;
+
+    private bool weaponOut = false;
+
+    //grab axis from parent object
+    public string button1;
+    public string button2;
+    public string button3;
+    public string button4; // currently boost in player move script
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -50,189 +68,207 @@ public class botB06_Ethan : MonoBehaviour
 		LeftParticles.SetActive(false);
 		RightParticles.SetActive(false);
 		BackParticles.SetActive(false);
+
+        //weapon buttons
+        button1 = gameObject.transform.parent.GetComponent<playerParent>().action1Input;
+        button2 = gameObject.transform.parent.GetComponent<playerParent>().action2Input;
+        button3 = gameObject.transform.parent.GetComponent<playerParent>().action3Input;
+        button4 = gameObject.transform.parent.GetComponent<playerParent>().action4Input;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.T)){
+        if ((Input.GetButtonDown(button1)) && (weaponOut == false))
+        {
+            FrontWeapon.transform.Translate(0, thrustAmount, 0);
+            weaponOut = true;
+            StartCoroutine(WithdrawWeapon());
+        }
     }
 
-	//taking Damage
-	//private void OnTriggerEnter(Collider other)
-	//{
-	//	if (other.gameObject.tag == "Hazard")
-	//	{
-	//		if ((other.gameObject.GetComponent<HazardDamage>().isPlayer1Weapon == false) && (thisPlayer == "Player1")) { notMyWeapon = true; }
-	//		else if ((other.gameObject.GetComponent<HazardDamage>().isPlayer2Weapon == false) && (thisPlayer == "Player2")) { notMyWeapon = true; }
-	//		else { notMyWeapon = false; }
-	//	}
+    IEnumerator WithdrawWeapon()
+    {
+        yield return new WaitForSeconds(0.6f);
+        FrontWeapon.transform.Translate(0, -thrustAmount, 0);
+        weaponOut = false;
+    }
 
-	//	if ((other.gameObject.tag == "Hazard") && (notMyWeapon == true))
-	//	{
-	//		attackDamage = other.gameObject.GetComponent<HazardDamage>().damage;
+    //taking Damage
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hazard")
+        {
+            if ((other.gameObject.GetComponent<HazardDamage>().isPlayer1Weapon == false) && (thisPlayer == "Player1")) { notMyWeapon = true; }
+            else if ((other.gameObject.GetComponent<HazardDamage>().isPlayer2Weapon == false) && (thisPlayer == "Player2")) { notMyWeapon = true; }
+            else { notMyWeapon = false; }
+        }
 
-	//		Vector3 directionFore = other.transform.position - transform.position;
-	//		Vector3 directionSides = other.transform.position - compassSides.transform.position;
-	//		Vector3 directionVert = other.transform.position - compassVertical.transform.position;
+        if ((other.gameObject.tag == "Hazard") && (notMyWeapon == true))
+        {
+            attackDamage = other.gameObject.GetComponent<HazardDamage>().damage;
 
-	//		if (Vector3.Dot(transform.forward, directionFore) < (-sidelimit))
-	//		{
-	//			rb.AddForce(transform.forward * knockBackSpeed * -1, ForceMode.Impulse);
-	//			//Debug.Log("HitBack " + Vector3.Dot (transform.forward, directionFore));
-	//			//if (shieldPowerBack <= 0)
-	//			//{
-	//			//	dmgParticlesBack.SetActive(true);
-	//			//	//string playerDamaged = gameObject.tag; //remove for final;
-	//			//	//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
-	//			//	gameHandler.TakeDamage(thisPlayer, attackDamage);  //use in final (slotted players)
-	//			//}
-	//			//else
-	//			//{
-	//			//	shieldPowerBack -= attackDamage;
-	//			//	StartCoroutine(ShieldHitDisplay(shieldBackObj));
-	//			//	if (shieldPowerBack <= 0)
-	//			//	{
-	//			//		shieldPowerBack = 0;
-	//			//		//string playerDamaged = gameObject.tag; //remove for final;
-	//			//		//gameHandler.PlayerShields(playerDamaged, "Back"); //remove for final;
-	//			//		gameHandler.PlayerShields(thisPlayer, "Back");  //use in final (slotted players)
-	//			//	}
-	//			//}
-	//		}
+            Vector3 directionFore = other.transform.position - transform.position;
+            Vector3 directionSides = other.transform.position - compassSides.transform.position;
+            Vector3 directionVert = other.transform.position - compassVertical.transform.position;
 
-	//		if (Vector3.Dot(transform.forward, directionFore) > sidelimit)
-	//		{
-	//			rb.AddForce(transform.forward * knockBackSpeed, ForceMode.Impulse);
-	//			//Debug.Log("HitFront "+ Vector3.Dot (transform.forward, directionFore));
-	//			//if (shieldPowerFront <= 0)
-	//			//{
-	//			//	dmgParticlesFront.SetActive(true);
-	//			//	//string playerDamaged = gameObject.tag; //remove for final;
-	//			//	//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
-	//			//	//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
-	//			//	gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
-	//			//}
-	//			//else
-	//			//{
-	//			//	shieldPowerFront -= attackDamage;
-	//			//	StartCoroutine(ShieldHitDisplay(shieldFrontObj));
-	//			//	if (shieldPowerFront <= 0)
-	//			//	{
-	//			//		shieldPowerFront = 0;
-	//			//		//string playerDamaged = gameObject.tag; //remove for final;
-	//			//		//gameHandler.PlayerShields(playerDamaged, "Front"); //remove for final;
-	//			//		gameHandler.PlayerShields(thisPlayer, "Front");  //use in final (slotted players)
-	//			//	}
-	//			//}
-	//		}
+            if (Vector3.Dot(transform.forward, directionFore) < (-sidelimit))
+            {
+                rb.AddForce(transform.forward * knockBackSpeed * -1, ForceMode.Impulse);
+                //Debug.Log("HitBack " + Vector3.Dot(transform.forward, directionFore));
+                if (shieldPowerBack <= 0)
+                {
+                    BackParticles.SetActive(true);
+                    //string playerDamaged = gameObject.tag; //remove for final;
+                    //gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+                    gameHandler.TakeDamage(thisPlayer, attackDamage);  //use in final (slotted players)
+                }
+                else
+                {
+                    shieldPowerBack -= attackDamage;
+                    StartCoroutine(ShieldHitDisplay(BackShield));
+                    if (shieldPowerBack <= 0)
+                    {
+                        shieldPowerBack = 0;
+                        //string playerDamaged = gameObject.tag; //remove for final;
+                        //gameHandler.PlayerShields(playerDamaged, "Back"); //remove for final;
+                        gameHandler.PlayerShields(thisPlayer, "Back");  //use in final (slotted players)
+                    }
+                }
+            }
 
-	//		if (Vector3.Dot(compassSides.transform.forward, directionSides) > sidelimit)
-	//		{
-	//			rb.AddForce(transform.right * knockBackSpeed, ForceMode.Impulse);
-	//			////Debug.Log("HitRight " + Vector3.Dot (compassSides.transform.forward, directionSides));
-	//			//if (shieldPowerRight <= 0)
-	//			//{
-	//			//	dmgParticlesRight.SetActive(true);
-	//			//	//string playerDamaged = gameObject.tag; //remove for final;
-	//			//	//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
-	//			//	//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
-	//			//	gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
-	//			//}
-	//			//else
-	//			//{
-	//			//	shieldPowerRight -= attackDamage;
-	//			//	StartCoroutine(ShieldHitDisplay(shieldRightObj));
-	//			//	if (shieldPowerRight <= 0)
-	//			//	{
-	//			//		shieldPowerRight = 0;
-	//			//		//string playerDamaged = gameObject.tag; //remove for final;
-	//			//		//gameHandler.PlayerShields(playerDamaged, "Right"); //remove for final;
-	//			//		gameHandler.PlayerShields(thisPlayer, "Right");  //use in final (slotted players)
-	//			//	}
-	//			//}
-	//		}
+            if (Vector3.Dot(transform.forward, directionFore) > sidelimit)
+            {
+                rb.AddForce(transform.forward * knockBackSpeed, ForceMode.Impulse);
+                //Debug.Log("HitFront "+ Vector3.Dot (transform.forward, directionFore));
+                //if (shieldPowerFront <= 0)
+                //{
+                //	dmgParticlesFront.SetActive(true);
+                //	//string playerDamaged = gameObject.tag; //remove for final;
+                //	//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+                //	//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+                //	gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+                //}
+                //else
+                //{
+                //	shieldPowerFront -= attackDamage;
+                //	StartCoroutine(ShieldHitDisplay(shieldFrontObj));
+                //	if (shieldPowerFront <= 0)
+                //	{
+                //		shieldPowerFront = 0;
+                //		//string playerDamaged = gameObject.tag; //remove for final;
+                //		//gameHandler.PlayerShields(playerDamaged, "Front"); //remove for final;
+                //		gameHandler.PlayerShields(thisPlayer, "Front");  //use in final (slotted players)
+                //	}
+                //}
+            }
 
-	//		if (Vector3.Dot(compassSides.transform.forward, directionSides) < (-sidelimit))
-	//		{
-	//			rb.AddForce(transform.right * knockBackSpeed * -1, ForceMode.Impulse);
-	//			//Debug.Log("HitLeft " + Vector3.Dot (compassSides.transform.forward, directionSides));
-	//			//if (shieldPowerLeft <= 0)
-	//			//{
-	//			//	dmgParticlesLeft.SetActive(true);
-	//			//	//string playerDamaged = gameObject.tag; //remove for final;
-	//			//	//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
-	//			//	//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
-	//			//	gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
-	//			//}
-	//			//else
-	//			//{
-	//			//	shieldPowerLeft -= attackDamage;
-	//			//	StartCoroutine(ShieldHitDisplay(shieldLeftObj));
-	//			//	if (shieldPowerLeft <= 0)
-	//			//	{
-	//			//		shieldPowerLeft = 0;
-	//			//		//string playerDamaged = gameObject.tag; //remove for final;
-	//			//		//gameHandler.PlayerShields(playerDamaged, "Left"); //remove for final;
-	//			//		gameHandler.PlayerShields(thisPlayer, "Left");  //use in final (slotted players)
-	//			//	}
-	//			//}
-	//		}
+            if (Vector3.Dot(compassSides.transform.forward, directionSides) > sidelimit)
+            {
+                rb.AddForce(transform.right * knockBackSpeed, ForceMode.Impulse);
+                //Debug.Log("HitRight " + Vector3.Dot (compassSides.transform.forward, directionSides));
+                if (shieldPowerRight <= 0)
+                {
+                    RightParticles.SetActive(true);
+                    //string playerDamaged = gameObject.tag; //remove for final;
+                    //Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+                    //gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+                    gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+                }
+                else
+                {
+                    shieldPowerRight -= attackDamage;
+                    StartCoroutine(ShieldHitDisplay(RightShield));
+                    if (shieldPowerRight <= 0)
+                    {
+                        shieldPowerRight = 0;
+                        //string playerDamaged = gameObject.tag; //remove for final;
+                        //gameHandler.PlayerShields(playerDamaged, "Right"); //remove for final;
+                        gameHandler.PlayerShields(thisPlayer, "Right");  //use in final (slotted players)
+                    }
+                }
+            }
 
-	//		if (Vector3.Dot(compassVertical.transform.forward, directionVert) > sidelimit)
-	//		{
-	//			rb.AddForce(transform.up * knockBackSpeed, ForceMode.Impulse);
-	//			//Debug.Log("HitTop " + Vector3.Dot (compassVertical.transform.forward, directionVert));
-	//			if (shieldPowerTop <= 0)
-	//			{
-	//				TopParticles.SetActive(true);
-	//				//string playerDamaged = gameObject.tag; //remove for final;
-	//				//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
-	//				//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
-	//				gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
-	//			}
-	//			else
-	//			{
-	//				shieldPowerTop -= attackDamage;
-	//				StartCoroutine(ShieldHitDisplay(TopShield));
-	//				if (shieldPowerTop <= 0)
-	//				{
-	//					shieldPowerTop = 0;
-	//					//string playerDamaged = gameObject.tag; //remove for final;
-	//					//gameHandler.PlayerShields(playerDamaged, "Top"); //remove for final;
-	//					gameHandler.PlayerShields(thisPlayer, "Top");  //use in final (slotted players)
-	//				}
-	//			}
-	//		}
+            if (Vector3.Dot(compassSides.transform.forward, directionSides) < (-sidelimit))
+            {
+                rb.AddForce(transform.right * knockBackSpeed * -1, ForceMode.Impulse);
+                //Debug.Log("HitLeft " + Vector3.Dot (compassSides.transform.forward, directionSides));
+                if (shieldPowerLeft <= 0)
+                {
+                    LeftParticles.SetActive(true);
+                    //string playerDamaged = gameObject.tag; //remove for final;
+                    //Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+                    //gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+                    gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+                }
+                else
+                {
+                    shieldPowerLeft -= attackDamage;
+                    StartCoroutine(ShieldHitDisplay(LeftShield));
+                    if (shieldPowerLeft <= 0)
+                    {
+                        shieldPowerLeft = 0;
+                        //string playerDamaged = gameObject.tag; //remove for final;
+                        //gameHandler.PlayerShields(playerDamaged, "Left"); //remove for final;
+                        gameHandler.PlayerShields(thisPlayer, "Left");  //use in final (slotted players)
+                    }
+                }
+            }
 
-	//		if (Vector3.Dot(compassVertical.transform.forward, directionVert) < (-sidelimit))
-	//		{
-	//			rb.AddForce(transform.up * knockBackSpeed * -1, ForceMode.Impulse);
-	//			//Debug.Log("HitBottom " + Vector3.Dot (compassVertical.transform.forward, directionVert));
-	//			if (shieldPowerBottom <= 0)
-	//			{
-	//				//dmgParticlesBottom.SetActive(true);
-	//				//string playerDamaged = gameObject.tag; //remove for final;
-	//				//Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
-	//				//gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
-	//				gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
-	//			}
-	//			else
-	//			{
-	//				shieldPowerBottom -= attackDamage;
-	//				if (shieldPowerBottom <= 0)
-	//				{
-	//					shieldPowerBottom = 0;
-	//					//string playerDamaged = gameObject.tag; //remove for final;
-	//					//gameHandler.PlayerShields(playerDamaged, "Bottom"); //remove for final;
-	//					gameHandler.PlayerShields(thisPlayer, "Bottom");  //use in final (slotted players)
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
+            if (Vector3.Dot(compassVertical.transform.forward, directionVert) > sidelimit)
+            {
+                rb.AddForce(transform.up * knockBackSpeed, ForceMode.Impulse);
+                //Debug.Log("HitTop " + Vector3.Dot (compassVertical.transform.forward, directionVert));
+                if (shieldPowerTop <= 0)
+                {
+                    TopParticles.SetActive(true);
+                    //string playerDamaged = gameObject.tag; //remove for final;
+                    //Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+                    //gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+                    gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+                }
+                else
+                {
+                    shieldPowerTop -= attackDamage;
+                    StartCoroutine(ShieldHitDisplay(TopShield));
+                    if (shieldPowerTop <= 0)
+                    {
+                        shieldPowerTop = 0;
+                        //string playerDamaged = gameObject.tag; //remove for final;
+                        //gameHandler.PlayerShields(playerDamaged, "Top"); //remove for final;
+                        gameHandler.PlayerShields(thisPlayer, "Top");  //use in final (slotted players)
+                    }
+                }
+            }
 
-	IEnumerator ShieldHitDisplay(GameObject shieldObj)
+            if (Vector3.Dot(compassVertical.transform.forward, directionVert) < (-sidelimit))
+            {
+                rb.AddForce(transform.up * knockBackSpeed * -1, ForceMode.Impulse);
+                //Debug.Log("HitBottom " + Vector3.Dot (compassVertical.transform.forward, directionVert));
+                if (shieldPowerBottom <= 0)
+                {
+                    //dmgParticlesBottom.SetActive(true);
+                    //string playerDamaged = gameObject.tag; //remove for final;
+                    //Debug.Log("I hit the core of " + playerDamaged + "\n for damage = " + attackDamage); // remove in final
+                    //gameHandler.TakeDamage(playerDamaged, attackDamage); //remove for final;
+                    gameHandler.TakeDamage(thisPlayer, attackDamage); // use in final (slotted players)
+                }
+                else
+                {
+                    shieldPowerBottom -= attackDamage;
+                    if (shieldPowerBottom <= 0)
+                    {
+                        shieldPowerBottom = 0;
+                        //string playerDamaged = gameObject.tag; //remove for final;
+                        //gameHandler.PlayerShields(playerDamaged, "Bottom"); //remove for final;
+                        gameHandler.PlayerShields(thisPlayer, "Bottom");  //use in final (slotted players)
+                    }
+                }
+            }
+        }
+    }
+
+    IEnumerator ShieldHitDisplay(GameObject shieldObj)
 	{
 		shieldObj.SetActive(true);
 		// Renderer shieldRenderer = GetComponent<Renderer> ();
